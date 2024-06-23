7ab948307a864f140164e98b1651ff687ebf2da0
Manisha <manishavkapex1@gmail.com>
2024-06-23 20:25:00 +0530

    search element

--- 01-Arrays/array-search.js
+++ 01-Arrays/array-search.js
@@ -9,7 +9,7 @@ Que : Search an element in array by index
 let arr = [10, 50, 47, 60, 89, 70, 99]
 let item = 60;
 let index = undefined;
-for(let i=0; i<arr.length -1; i++){
+for(let i=0; i<=arr.length -1; i++){
     // check jab loop chal rha ho to kya arr me i ki value match ho rha hai item ke saths
     if(arr[i] === item){
         index = i;
